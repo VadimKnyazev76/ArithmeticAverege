967799de8c20da47affc0c496e08a44beb7dcac7
VadimKnyazev76 <vadim.knyazev@gmail.com>
2021-01-24 22:41:56 +0200

    ArithmeticAverage

---
+++ src/ArithmeticsAverage.java
@@ -0,0 +1,14 @@
+import com.sun.org.glassfish.external.statistics.AverageRangeStatistic;
+
+public class ArithmeticsAverage {
+
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 7;
+        int c = 2;
+        int average = (a+b+c)/3;
+
+        System.out.println(average);
+    }
+}
